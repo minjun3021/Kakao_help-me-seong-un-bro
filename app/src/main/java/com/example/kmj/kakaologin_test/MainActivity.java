0cf0cca6947739d28d414fe4f4e89fe1f43db40f
SunSet0418 <coffee@soylatte.kr>
2018-09-14 10:47:39 +0900

    Kakao Account peristalsis successful

--- app/src/main/java/com/example/kmj/kakaologin_test/MainActivity.java
+++ app/src/main/java/com/example/kmj/kakaologin_test/MainActivity.java
@@ -2,6 +2,7 @@
 
 package com.example.kmj.kakaologin_test;
 
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
@@ -9,34 +10,81 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
+import android.view.inputmethod.InputMethod;
+
+import com.kakao.auth.AccessTokenCallback;
+import com.kakao.auth.ISessionCallback;
+import com.kakao.auth.Session;
+import com.kakao.auth.authorization.accesstoken.AccessTokenListener;
+import com.kakao.network.ErrorResult;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.MeV2ResponseCallback;
+import com.kakao.usermgmt.response.MeV2Response;
+import com.kakao.util.exception.KakaoException;
+import com.kakao.util.helper.log.Logger;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SessionCallback callback;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getHashKey();
+
+        callback = new SessionCallback();
+
+        Session.getCurrentSession().addCallback(callback);
+
     }
-    private void getHashKey(){
-        try {
-            PackageInfo info = getPackageManager().getPackageInfo("com.example.kmj.kakaologin_test", PackageManager.GET_SIGNATURES);
-            for (Signature signature : info.signatures) {
-                MessageDigest md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                Log.e("asdf","key_hash="+ Base64.encodeToString(md.digest(), Base64.DEFAULT)+"1234");
-            }
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (Session.getCurrentSession().handleActivityResult(requestCode, resultCode, data)) {
+            return;
         }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Session.getCurrentSession().removeCallback(callback);
+    }
 
+    private class SessionCallback implements ISessionCallback {
 
+        @Override
+        public void onSessionOpened() {
+            Log.i("KAKAO", "Session Opened");
+            requsetMe();
+        }
 
+        @Override
+        public void onSessionOpenFailed(KakaoException exception) {
+            if(exception != null) {
+                Logger.e(exception);
+            }
+        }
+
+    }
+
+    public void requsetMe(){
+        UserManagement.getInstance().me(new MeV2ResponseCallback() {
+            @Override
+            public void onSessionClosed(ErrorResult errorResult) {
+
+            }
+
+            @Override
+            public void onSuccess(MeV2Response result) {
+                // TODO 나중에 서버 연동할떄 서버에 보낼 access_token값
+                String access_token = Session.getCurrentSession().getTokenInfo().getAccessToken();
+                Log.e("token", access_token);
+            }
+        });
+    }
 }
