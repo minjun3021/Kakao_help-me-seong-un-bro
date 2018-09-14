fe663f623d98d1b7180db5380d7262ec2121e56d
minjun3021 <minjun3021@naver.com>
2018-09-14 09:12:31 +0900

    first

---
+++ app/src/main/java/com/example/kmj/kakaologin_test/MainActivity.java
@@ -0,0 +1,42 @@
+
+
+package com.example.kmj.kakaologin_test;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getHashKey();
+    }
+    private void getHashKey(){
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo("com.example.kmj.kakaologin_test", PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.e("asdf","key_hash="+ Base64.encodeToString(md.digest(), Base64.DEFAULT)+"1234");
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+}
